3071ad2941b5cd881f5e297c027e922f6d2171b7
Madcros <madcros@gmail.com>
2016-01-31 15:08:52 +0200

    Add ?completed filter

---
+++ variable_playground.js
@@ -0,0 +1,33 @@
+/**
+ * Created by madvic on 1/30/16.
+ */
+
+//var person = {
+//    name: 'Viktor',
+//    age: 35
+//};
+//
+//function updatePerson (obj) {
+//    //obj = {
+//    //    name: 'Viktor',
+//    //    age: 24
+//    //};
+//
+//    obj.age = 24;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+// Array Example
+
+var grades = [15, 50];
+
+function addGrades (gradesNew) {
+    gradesNew.push(55);
+    debugger;
+}
+
+
+addGrades(grades);
+console.log(grades);
