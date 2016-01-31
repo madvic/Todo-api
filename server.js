2da33512bbabf4b155a14f9fb6a98ce6d7c9985a
Madcros <madcros@gmail.com>
2016-02-01 00:12:19 +0200

    final static

--- server.js
+++ server.js
@@ -29,7 +29,13 @@ app.get('/todos', function (req, res) {
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === "false") {
         filteredTodos = _.where(filteredTodos, {completed: false});
     }
-    
+
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function (todo) {
+           return todo.description.toLowerCase().indexOf(queryParams.q.toLocaleLowerCase()) > -1;
+        });
+    }
+
     res.json(filteredTodos);
 });
 
